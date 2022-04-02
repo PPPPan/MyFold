0ebe9dd3213207623286ac91d1cbddf653b4e281
PPPPan <623166492@qq.com>
2022-04-02 18:33:07 +0200

    test

--- update.sh
+++ update.sh
@@ -8,5 +8,5 @@ git add *
 read -p "Comment:" c
 git commit -m "$c"
 git remote add origin git@github.com:PPPPan/MyFold.git
-git branch -M main
-git push -u origin main
+git branch -M test
+git push -u origin test
