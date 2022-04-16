1db93fd3b1de14cf8c67885e1366554242cad996
PPPPan <623166492@qq.com>
2022-04-16 18:54:08 +0200

    new

--- gitConf.sh
+++
@@ -1,2 +0,0 @@
-ssh-keygen -t rsa -C "623166492@qq.com"
-cat ~/.ssh/id_rsa.pub
--- gitGlobalConf.sh
+++
@@ -1,3 +0,0 @@
-git config --global user.name "PPPPan"
-git config --global user.email "623166492@qq.com"
-git config --global -l
